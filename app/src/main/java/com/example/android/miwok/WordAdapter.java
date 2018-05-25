1ee1f4cf9e3abdaadb0e6633a1150a60127c0d29
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-05-25 16:23:05 +0100

    Use WordAdapter to populate list of words

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by McDermott on 24/05/2018.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(@NonNull Context context, @NonNull List<Word> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Word currentWord = getItem(position);
+
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView defaultTranslationTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTranslationTextView.setText(currentWord.getDefaultTranslation());
+
+        TextView miwokTranslationTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTranslationTextView.setText(currentWord.getMiwokTranslation());
+
+        return listItemView;
+    }
+}
