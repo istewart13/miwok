15285be0acdc976cee9e6f6629f67f6b3dbf7065
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-05-28 13:50:46 +0100

    Update word class to add image and second constructor

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,10 +8,17 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceID;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+        this.mDefaultTranslation = defaultTranslation;
+        this.mMiwokTranslation = miwokTranslation;
+        this.mImageResourceID = imageResourceID;
     }
 
     public String getDefaultTranslation() {
@@ -20,5 +27,9 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceID() {
+        return mImageResourceID;
     }
 }
